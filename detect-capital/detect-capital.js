1e4dd58bd09e4d122f26c9403568db975fe84988
Jeremy Ng <11280413+KingJeremyNg@users.noreply.github.com>
2021-01-20 16:01:40 -0500

    Time: 84 ms (89.01%), Space: 39.9 MB (57.80%) - LeetHub

---
+++ detect-capital/detect-capital.js
@@ -0,0 +1,12 @@
+/**
+ * @param {string} word
+ * @return {boolean}
+ */
+var detectCapitalUse = function(word) {
+    if (word.toUpperCase() === word) return true;
+    if (word.toLowerCase() === word) return true;
+    if (word[0].toUpperCase() === word[0] && word.slice(1, word.length).toLowerCase() === word.slice(1, word.length)) {
+        return true;
+    }
+    return false;
+};
