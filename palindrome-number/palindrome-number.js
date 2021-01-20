79de893b56e081fe6ebdf7811380bfc965d5544a
Jeremy Ng <11280413+KingJeremyNg@users.noreply.github.com>
2021-01-20 15:46:49 -0500

    Time: 208 ms (85.71%), Space: 49.4 MB (15.33%) - LeetHub

---
+++ palindrome-number/palindrome-number.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number} x
+ * @return {boolean}
+ */
+var isPalindrome = function(x) {
+    if (x < 0) return false;
+    string = x.toString();
+    for (let i = 0; i < string.length / 2; i++) {
+        if (string[i] != string[string.length - i - 1]) return false;
+    }
+    return true;
+};
