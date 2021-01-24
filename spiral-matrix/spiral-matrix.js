83345fb12f47dd0f88b0b339338e24667e483127
KingJeremyNg <jeremy97ng@gmail.com>
2021-01-24 17:00:20 -0500

    Time: 84 ms (19.95%), Space: 40.6 MB (6.26%) - FIX

--- spiral-matrix/spiral-matrix.js
+++ spiral-matrix/spiral-matrix.js
@@ -1,46 +1,60 @@
-            
-            // Downward case
-            case 1:
-                if (matrix.length === 1) {
-                    ans.push(matrix[0].pop());
-                    break;
-                }
-                else if (matrix[0].length === 1) {
-                    while (matrix.length != 0) {
-                        ans.push(matrix.shift());
-                    }
-                }
-                else {
-                    for (let i = 0; i < matrix.length; i++) {
-                        ans.push(matrix[i].pop());
-                    }
-                }
-                break;
-            
-            // Left case
-            case 2:
-                ans.push(...matrix.pop().reverse());
-                break;
-            
-            // Upward case
-            case 3:
-                if (matrix.length === 1) {
-                    ans.push(matrix[0].shift());
-                    break;
-                }
-                else if (matrix[0].length === 1) {
-                    while (matrix.length != 0) {
-                        ans.push(matrix.pop());
-                    }
-                }
-                else {
-                    for (let i = matrix.length - 1; i >= 0; i--) {
-                        ans.push(matrix[i].shift());
-                    }
-                }
-                break;
-        }
-        direction++;
-    }
-    return ans;
+/**
+ * @param {number[][]} matrix
+ * @return {number[]}
+ */
+var spiralOrder = function(matrix) {
+    let direction = 0;
+    let ans = [];
+    while (matrix.length != 0) {
+        switch(direction % 4) {
+            
+            // Right case
+            case 0:
+                ans.push(...matrix.shift());
+                break;
+            
+            // Downward case
+            case 1:
+                if (matrix.length === 1) {
+                    ans.push(matrix[0].pop());
+                    break;
+                }
+                else if (matrix[0].length === 1) {
+                    while (matrix.length != 0) {
+                        ans.push(matrix.shift());
+                    }
+                }
+                else {
+                    for (let i = 0; i < matrix.length; i++) {
+                        ans.push(matrix[i].pop());
+                    }
+                }
+                break;
+            
+            // Left case
+            case 2:
+                ans.push(...matrix.pop().reverse());
+                break;
+            
+            // Upward case
+            case 3:
+                if (matrix.length === 1) {
+                    ans.push(matrix[0].shift());
+                    break;
+                }
+                else if (matrix[0].length === 1) {
+                    while (matrix.length != 0) {
+                        ans.push(matrix.pop());
+                    }
+                }
+                else {
+                    for (let i = matrix.length - 1; i >= 0; i--) {
+                        ans.push(matrix[i].shift());
+                    }
+                }
+                break;
+        }
+        direction++;
+    }
+    return ans;
 };
