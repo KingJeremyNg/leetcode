0450754145f59b106dca1b8c7b108d09ffb6d3a3
Jeremy Ng <11280413+KingJeremyNg@users.noreply.github.com>
2021-02-04 18:13:25 -0500

    Time: 108 ms (86.93%), Space: 43.3 MB (95.74%) - LeetHub

---
+++ sort-array-by-parity-ii/sort-array-by-parity-ii.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} A
+ * @return {number[]}
+ */
+var sortArrayByParityII = function(A) {
+    let j = 1;
+    for (let i = 0; i < A.length; i += 2) {
+        if (A[i] % 2) {
+            while (A[j] % 2) j += 2;
+            let temp = A[i];
+            A[i] = A[j];
+            A[j] = temp;
+        }
+    }
+    return A;
+};
