c0dff56bc80f2bb1781132d3d874f3e76df6aa5c
KingJeremyNg <jeremy97ng@gmail.com>
2021-02-06 22:54:43 -0500

    Time: 76 ms (84.68%), Space: 38.4 MB (88.71%) - Resubmit

--- path-crossing/path-crossing.js
+++ path-crossing/path-crossing.js
@@ -5,7 +5,7 @@
 var isPathCrossing = function(path) {
     let y = 0;
     let x = 0;
-    let points = [[0, 0]];
+    let points = new Set().add(`${x}${y}`);
     for (let i = 0; i < path.length; i++) {
         switch (path[i]) {
             case "N":
@@ -21,14 +21,8 @@ var isPathCrossing = function(path) {
                 x--;
                 break;
         }
-        if (check(points, x, y)) return true;
+        if (points.has(`${x}${y}`)) return true;
+        else points.add(`${x}${y}`);
     }
     return false;
 };
-
-const check = (points, x, y) => {
-    let a = JSON.stringify(points);
-    let b = JSON.stringify([x, y]);
-    if (a.indexOf(b) !== -1) return true;
-    else points.push([x, y]);
-}
