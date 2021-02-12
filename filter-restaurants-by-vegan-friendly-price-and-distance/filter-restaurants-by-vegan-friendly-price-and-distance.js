c7c4bf089e94d57c243fe55debb6194023385a7e
Jeremy Ng <11280413+KingJeremyNg@users.noreply.github.com>
2021-02-12 14:48:31 -0500

    Time: 124 ms (28.89%), Space: 47.6 MB (11.11%) - LeetHub

---
+++ filter-restaurants-by-vegan-friendly-price-and-distance/filter-restaurants-by-vegan-friendly-price-and-distance.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[][]} restaurants
+ * @param {number} veganFriendly
+ * @param {number} maxPrice
+ * @param {number} maxDistance
+ * @return {number[]}
+ */
+var filterRestaurants = function(restaurants, veganFriendly, maxPrice, maxDistance) {
+    let arr = restaurants.filter(([id, rating, vegan, price, distance]) =>
+        (!veganFriendly || vegan) &&
+            price <= maxPrice &&
+            distance <= maxDistance
+    )
+    console.log(arr);
+    arr.sort((a, b) => {
+        if (a[1] - b[1] !== 0) return b[1] - a[1];
+        else return b[0] - a[0];
+    })
+    return arr.map((element) => {return element[0]});
+};
